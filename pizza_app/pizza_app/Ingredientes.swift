aa2e40a7d1259a14e6601e85ba5788c28e4326ea
Francisco Manuel Romero <fran@frans-pro.local>
2016-04-17 23:57:24 +0200

    Added files

--- pizza_app/pizza_app/Ingredientes.swift
+++ pizza_app/pizza_app/Ingredientes.swift
@@ -10,12 +10,65 @@ import UIKit
 
 class Ingredientes: UIViewController {
 
+    func checkIngredients() -> (number:Int, text:String){
+        var ingr:String = "";
+        var number : Int = 0;
+        if (jamon.isChecked) {
+            ingr+=(jamon.titleLabel?.text)!+"\n";
+            number++;
+        }
+        if (pimiento.isChecked) {
+            ingr+=(pimiento.titleLabel?.text)!+"\n";
+            number++;
+        }
+        if (cebolla.isChecked) {
+            ingr+=(cebolla.titleLabel?.text)!+"\n";
+            number++;
+        }
+        if (aceituna.isChecked) {
+            ingr+=(aceituna.titleLabel?.text)!+"\n";
+            number++;
+        }
+        if (salchicha.isChecked) {
+            ingr+=(salchicha.titleLabel?.text)!+"\n";
+            number++;
+        }
+        if (pavo.isChecked) {
+            ingr+=(pavo.titleLabel?.text)!+"\n";
+            number++;
+        }
+        if (pepperoni.isChecked) {
+            ingr+=(pepperoni.titleLabel?.text)!+"\n";
+            number++;
+        }
+        return (number, ingr);
+    }
+
+    @IBOutlet weak var warning: UILabel!
+    @IBAction func nextAction(sender: AnyObject) {
+        print("pressed\n");
+        let ing = checkIngredients();
+        if (ing.number > 0 && ing.number < 6) {
+            let conf = self.storyboard!.instantiateViewControllerWithIdentifier("Confirmacion") as! Confirmacion
+            
+            conf.masa = self.masa;
+            conf.cheese = self.cheese;
+            conf.size = self.size;
+            conf.ingredients = ing.text;
+            warning.text = "";
+            self.navigationController!.pushViewController(conf, animated: true)
+        } else {
+            warning.text = "Escoge de 1 a 5 ingredientes";
+        }
+    }
+
     @IBOutlet weak var jamon: CheckBox!
     @IBOutlet weak var cebolla: CheckBox!
     @IBOutlet weak var aceituna: CheckBox!
     @IBOutlet weak var salchicha: CheckBox!
     @IBOutlet weak var pavo: CheckBox!
     @IBOutlet weak var pepperoni: CheckBox!
+
     @IBOutlet weak var pimiento: CheckBox!
     var masa : String = "";
     var cheese : String = "";
@@ -23,7 +76,6 @@ class Ingredientes: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -32,39 +84,12 @@ class Ingredientes: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+    /*override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         let svc = segue.destinationViewController as! Confirmacion;
-        svc.masa = self.masa;
-        svc.cheese = self.cheese;
-        svc.size = self.size;
-        var ingr = "";
-        if (jamon.isChecked) {
-            ingr+=(jamon.titleLabel?.text)!+"\n";
-        }
-        if (pimiento.isChecked) {
-            ingr+=(pimiento.titleLabel?.text)!+"\n";
-        }
-        if (cebolla.isChecked) {
-            ingr+=(cebolla.titleLabel?.text)!+"\n";
-        }
-        if (aceituna.isChecked) {
-            ingr+=(aceituna.titleLabel?.text)!+"\n";
-        }
-        if (salchicha.isChecked) {
-            ingr+=(salchicha.titleLabel?.text)!+"\n";
-        }
-        if (pavo.isChecked) {
-            ingr+=(pavo.titleLabel?.text)!+"\n";
-        }
-        if (pepperoni.isChecked) {
-            ingr+=(pepperoni.titleLabel?.text)!+"\n";
-        }
-        svc.ingredients = ingr;
         print("size is:\(size)\n");
         print("cheese is:\(cheese)\n");
         print("masa is:\(masa)\n");
-        print("ingr is:\(ingr)\n");
-    }
+    }*/
 
     /*
     // MARK: - Navigation
